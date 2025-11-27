37b9e495d8745e9e9225da3b1ced254bf42ffb86
Refilwe15 <mrefilwe880@gmail.com>
2025-11-27 20:56:07 +0200

    done with login page

--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,17 +1,51 @@
-import '../styles/loginPage.css';
-import loginPic from '../assets/login2.png';
-
+import "../styles/loginPage.css";
+import loginPic from "../assets/login2.png";
 
 function Login() {
-  
-
   return (
-    <>
-    <div className='container'>
-    <img src={loginPic} alt="Logo" className="pic"/>
+    <div className="login-wrapper">
+      <div className="card">
+        
+        <div className="left">
+          <img
+            src={loginPic}
+            alt="Login Illustration"
+            className="login-image"
+          />
+        </div>
 
+        
+        <div className="right">
+          <h1>Welcome Back!</h1>
+          <p className="subtitle">
+            Login to access your account and all features.
+          </p>
+
+          <input type="text" placeholder="email" className="input-field" />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input-field"
+          />
+
+          <div className="options">
+            <label>
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="#" className="forgot">
+              Forgot password?
+            </a>
+          </div>
+
+          <button className="login-btn">Log In</button>
+
+          <p className="register">
+            Don't have account ? <a href="#">Register here</a>
+          </p>
+        </div>
+      </div>
     </div>
-    </>
-  )
+  );
 }
-export default Login
+
+export default Login;
